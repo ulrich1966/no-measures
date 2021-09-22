f95bd531ec6e66f0aa932e98886784ced2a8c181
Ulrich1966 <post2uli@gmx.de>
2021-09-22 14:16:59 +0200

    add fol form Harry

--- script/objects.js
+++ script/objects.js
@@ -1,83 +1,181 @@
-img_objects = [ {
-		name : "img/gorilla.jpg",
-		alt : "bild 1",
-		lbl : "Toller Gorilla",
-		txt : "Ein geimpfter Gorilla, dem es gerade nicht gut geht.",
-		colorinx: 0,
-		link : {
-			href : "public/dummy.html",
-			txt : "Toller Gorilla"
-		}
-	}, {
-		name : "img/fol_gesundheit.svg",
-		alt : "bild 2",
-		lbl : "Gesunfheit ist biologisch",
-		txt : "An menschlichem Erbgut daft nicht manipoliet werden! Finger weg von mRNA Impfstoffen!",
-		colorinx: 1,
-		link : {
-			href : "public/dummy.html",
-			txt : "Gesunfheit ist biologisch"
-		}
-	}, {
-		name : "img/fol_hisory.svg",
-		alt : "bild 3",
-		lbl : "Aus der Geschichte lernen",
-		txt : "Wer seine Geschischte nicht kennt, ist dazu verdamt deren Fehler zu wiederholen!",
-		colorinx: 2,
-		link : {
-			href : "public/dummy.html",
-			txt : "Aus der Geschichte lernen"
-		}
-	}, {
-		name : "img/fol_impffalle.svg",
-		alt : "bild 4",
-		lbl : "Achtung vor der Drückerkolle!",
-		txt : "Die Bratwurst ist der Köder. Greifen Sie NICHT zu oder sie sitzen in der Falle!",
-		colorinx: 3,
-		link : {
-			href : "public/dummy.html",
-			txt : "Achtung vor der Drückerkolle!"
-		}
-	}, {
-		name : "img/fol_krouption.svg",
-		alt : "bild 5",
-		lbl : "Geld regiert die Welt ...",
-		txt : "... Aber wir brauchen endlich eine Politik, in der die monitären Interssen einzelner nicht im Vordergrund stehen dürfen.",
-		colorinx: 0,
-		link : {
-			href : "public/dummy.html",
-			txt : "Geld regiert die Welt"
-		}
-	}, {
-		name : "img/fol_massnahmen.svg",
-		alt : "bild 6",
-		lbl : "Genig ist Genug",
-		txt : "Um unsere Gesellschaft, unsere Demokratie, unsere Freiheit und unseren Rechtsstaat zu retten müssen alle Corona-Maßnahmen sofort beendet werden.",
-		colorinx: 1,
-		link : {
-			href : "public/dummy.html",
-			txt : "Genig ist Genug"
-		}
-	}, {
-		name : "img/fol_spaltung.svg",
-		alt : "bild 7",
-		lbl : "Divide et impera",
-		txt : "Wer sich spalten lässt, lässt sich leichter regieren. Totilitarismus im Gewand der Solidaritä ist keine itegrative Maßmahmen für eine Gesellschaft.",
-		colorinx: 2,
-		link : {
-			href : "public/dummy.html",
-			txt : "Divide et impera"
-		}
-	}, {
-		name : "img/fol_waehlen.svg",
-		alt : "bild 8",
-		lbl : "Divide et impera",
-		txt : "Wer sich spalten lässt, lässt sich leichter regieren. Totilitarismus im Gewand der Solidaritä ist keine itegrative Maßmahmen für eine Gesellschaft.",
-		colorinx: 3,
-		link : {
-			href : "public/dummy.html",
-			txt : "Divide et impera"
-		}
-	} ];
-	
-colors = [{ name: "green", value: "#2b9c38" }, { name: "blue", value: "#dc0c22"}, { name: "red", value: "#233c7a"}, { name: "orange", value: "#f29200"}];
+img_objects = [{
+	name: "img/gorilla.jpg",
+	alt: "bild 1",
+	lbl: "Toller Gorilla",
+	txt: "Ein geimpfter Gorilla, dem es gerade nicht gut geht.",
+	colorinx: 0,
+	link: {
+		href: "public/dummy.html",
+		txt: "Toller Gorilla"
+	}
+}, {
+	name: "img/fol_gesundheit.svg",
+	alt: "bild 2",
+	lbl: "Gesunfheit ist biologisch",
+	txt: "An menschlichem Erbgut daft nicht manipoliet werden! Finger weg von mRNA Impfstoffen!",
+	colorinx: 1,
+	link: {
+		href: "public/dummy.html",
+		txt: "Gesunfheit ist biologisch"
+	}
+}, {
+	name: "img/fol_hisory.svg",
+	alt: "bild 3",
+	lbl: "Aus der Geschichte lernen",
+	txt: "Wer seine Geschischte nicht kennt, ist dazu verdamt deren Fehler zu wiederholen!",
+	colorinx: 2,
+	link:  {
+		href: "public/dummy.html",
+		txt: "Aus der Geschichte lernen"
+	}
+}, {
+	name: "img/fol_impffalle.svg",
+	alt: "bild 4",
+	lbl: "Achtung vor der Drückerkolle!",
+	txt: "Die Bratwurst ist der Köder. Greifen Sie NICHT zu oder sie sitzen in der Falle!",
+	colorinx: 3,
+	link: {
+		href: "public/dummy.html",
+		txt: "Achtung vor der Drückerkolle!"
+	}
+}, {
+	name: "img/fol_krouption.svg",
+	alt: "bild 5",
+	lbl: "Geld regiert die Welt ...",
+	txt: "... Aber wir brauchen endlich eine Politik, in der die monitären Interssen einzelner nicht im Vordergrund stehen dürfen.",
+	colorinx: 0,
+	link: {
+		href: "public/dummy.html",
+		txt: "Geld regiert die Welt"
+	}
+}, {
+	name: "img/fol_massnahmen.svg",
+	alt: "bild 6",
+	lbl: "Genig ist Genug",
+	txt: "Um unsere Gesellschaft, unsere Demokratie, unsere Freiheit und unseren Rechtsstaat zu retten müssen alle Corona-Maßnahmen sofort beendet werden.",
+	colorinx: 1,
+	link: {
+		href: "public/dummy.html",
+		txt: "Genig ist Genug"
+	}
+}, {
+	name: "img/fol_spaltung.svg",
+	alt: "bild 7",
+	lbl: "Divide et impera",
+	txt: "Wer sich spalten lässt, lässt sich leichter regieren. Totilitarismus im Gewand der Solidaritä ist keine itegrative Maßmahmen für eine Gesellschaft.",
+	colorinx: 2,
+	link: {
+		href: "public/dummy.html",
+		txt: "Divide et impera"
+	} 
+}, {
+	name: "img/fol_waehlen.svg",
+	alt: "bild 8",
+	lbl: "Du hast die Wahl",
+	txt: "Entscheide Dich am 26.09.21 für Dich, entscheide Dich für die Basis!",
+	colorinx: 3,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Divide et impera"
+	}
+}, {
+	name: "img/fol_antifa.png",
+	alt: "bild 8",
+	lbl: "Corona ist nich der Feind",
+	txt: "Wir bekämpfen den Faschissmus mit direkter Demokratie!",
+	colorinx: 0,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Divide et impera"
+	}
+}, {
+	name: "img/fol_armut.png",
+	alt: "bild 8",
+	lbl: "Armut ist keine Schande ...",
+	txt: "... Nur die reichsten Länder können sich Armut leisten.",
+	colorinx: 1,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Antifa"
+	}
+}, {
+	name: "img/fol_kinder.png",
+	alt: "bild 8",
+	lbl: "Kinder sind Corana resitent",
+	txt: "Gebt Euren Kindern die Chance auf eine natürliche Entwicklung.",
+	colorinx: 2,
+	link: {
+		href:  "public/dummy.html",
+		txt : "kinder"
+	}
+}, {
+	name: "img/fol_medien.png",
+	alt: "bild 8",
+	lbl: "Meinungfreiheit für alle",
+	txt: "Hast Du Dich schon mal gefragt, warum einige Medien ihre Meinung verbreiten dürfen und andere nicht?",
+	colorinx: 3,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Medien"
+	}
+}, {
+	name: "img/fol_wohnraum.png",
+	alt: "bild 8",
+	lbl: "Zuhause ist es immer noch am schönsten",
+	txt: "Wenn Du morgen noch ein Zuhause haben willst, dann darf Wohnraum kein Spekulationsobjekt sein.",
+	colorinx: 0,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Divide et impera"
+	}
+}, {
+	name: "img/fol_zuhause.png",
+	alt: "bild 8",
+	lbl: "Wonung ist Grundbedürfnis",
+	txt: "Jeder muss einen persölichen Lebensraum haben.",
+	colorinx: 1,
+	link: {
+		href:  "public/dummy.html",
+		txt : "Wohnraum"
+	}
+}];
+
+mov_objects = [{
+	name: "mov/film-1.mp4",
+	lbl: "Film 1",
+	txt: "Worum geht es?",
+	colorinx: 0,
+	link: {
+		href: "public/dummy.html?film-1.mp4",
+		txt: "Film 1"
+	}	
+}, {
+	name: "mov/film-2.mp4",
+	lbl: "Film 2",
+	txt: "Worum geht es?",
+	colorinx: 1,
+	link: {
+		href: "public/dummy.html?film-2.mp4",
+		txt: "Film 2"
+	}
+}, {
+	name: "mov/film-3.mp4",
+	lbl: "Film 3",
+	txt: "Worum geht es?",
+	colorinx: 2,
+	link: {
+		href: "public/dummy.html?film-3.mp4",
+		txt: "Film 3"
+	}
+}, {
+	name: "mov/film-4.mp4",
+	lbl: "Film 3",
+	txt: "Worum geht es?",
+	colorinx: 3,
+	link: {
+		href: "public/dummy.html?film-3.mp4",
+		txt: "Film 3"
+	}
+}];
+
+colors = [{ name: "green", value: "#2b9c38" }, { name: "blue", value: "#dc0c22" }, { name: "red", value: "#233c7a" }, { name: "orange", value: "#f29200" }];
